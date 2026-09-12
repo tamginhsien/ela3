db5b856c8a52363e3bf17eeb4cd09e3fc5cf33df
ginhsien <ginhsien299@gmail.com>
2026-09-13 03:31:06 +0800

    chore: strictly remove sensitive and temp files with fixed gitignore

--- fix_quotes.py
+++
@@ -1,7 +0,0 @@
-for i in [1, 2, 3]:
-    filename = f'app/src/main/res/layout/activity_submenu3{i}.xml'
-    with open(filename, 'r', encoding='utf-8') as f:
-        xml = f.read()
-    xml = xml.replace('android:id=\\"@+id/codeEx', 'android:id="@+id/codeEx')
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(xml)
